cee3c94835643e158cd945f808a31ea9fa99ca02
harisheoran <harisheoran@protonmail.com>
2025-02-01 13:46:51 +0530

    mulitthreaded working proxy without cache

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -31,58 +32,60 @@ func main() {
 	for {
 		// accept the connection
 		connection, err := listener.Accept()
-		defer connection.Close()
 		app.handleError("failed to accepet connection", err)
 
-		// read the request
-		reader := bufio.NewReader(connection)
-		request, err := http.ReadRequest(reader)
-		app.handleError("failed to prase the http request", err)
-
-		// check the request
-		isValid := app.ValidateRequest(request)
-		if !isValid {
-			app.infoLogger.Println("Not a valid request", request.RequestURI)
-		}
-		// forward the request
-		response, err := app.HandleRequest(request)
-		app.handleError("failed to forward the request", err)
-
-		fmt.Println("reading the responsebody", response)
-
-		// preparing the response to send
-		// 1. body
-
-		body, err := io.ReadAll(response.Body)
-		app.handleError("failed to read response body", err)
-
-		// 2. headers
-		var headerToSend string
-		for key, value := range response.Header {
-			headerToSend = headerToSend + fmt.Sprintf("%s: %s\r\n", key, value[0])
-		}
-
-		// 3. final response
-		responseToSend := fmt.Sprintf("%s %s\r\n", response.Proto, response.Status) +
-			headerToSend
-
-		connection.Write([]byte(responseToSend))
-		connection.Write([]byte("\r\n")) // End of headers
-		connection.Write(body)
-		connection.Close()
+		go app.handleConnection(connection)
 	}
 
 }
 
-func (app *app) ValidateRequest(request *http.Request) bool {
-	if request.Host == "" {
-		return false
+func (app *app) handleConnection(connection net.Conn) {
+	defer connection.Close()
+	// read the request
+	reader := bufio.NewReader(connection)
+	request, err := http.ReadRequest(reader)
+	if err != nil {
+		app.errorLogger.Println("Failed to parse the request:", err)
+		connection.Close()
+		return
 	}
 
-	return true
+	// check the validity of the request
+
+	// forward the request
+	response, err := app.HandleRequestManual(request)
+	if err != nil {
+		app.errorLogger.Println("failed tp handle the request from the client", err)
+		return
+	}
+	app.handleError("failed to forward the request", err)
+
+	response.Write(connection)
+	defer response.Body.Close()
 }
 
 func (app *app) HandleRequest(request *http.Request) (*http.Response, error) {
+	newRequest, err := http.NewRequest(request.Method, request.URL.String(), request.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	newRequest.Header = request.Header
+
+	client := &http.Client{}
+	response, err := client.Do(newRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	app.infoLogger.Println("Request forwarded successfully")
+	return response, nil
+
+}
+
+// handle request manually
+func (app *app) HandleRequestManual(request *http.Request) (*http.Response, error) {
+
 	// create connection
 	connection, err := net.Dial("tcp", request.Host)
 	if err != nil {
@@ -90,24 +93,61 @@ func (app *app) HandleRequest(request *http.Request) (*http.Response, error) {
 	}
 
 	// prepare request to send
-	requestToSend := fmt.Sprintf("GET %s HTTP/1.1\r\n", request.URL.Path) +
+	// 1. headers
+	var headersToSend string
+	for key, value := range request.Header {
+		headersToSend = headersToSend + fmt.Sprintf("%s: %s\r\n", key, strings.Join(value, ","))
+	}
+	// 2. body
+	bodyToSend, err := io.ReadAll(request.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. combine all request
+	requestToSend := fmt.Sprintf("%s %s %s\r\n", request.Method, request.URL.Path, request.Proto) +
 		fmt.Sprintf("Host: %s\r\n", request.Host) +
-		"\r\n"
+		headersToSend +
+		"\r\n" +
+		string(bodyToSend)
+
+		//	fmt.Println("FULL REQUEST:\n", requestToSend)
 
 	// send the request
-	connection.Write([]byte(requestToSend))
+	_, err = connection.Write([]byte(requestToSend))
+	if err != nil {
+		return nil, err
+	}
 
+	// read the response
 	reader := bufio.NewReader(connection)
 	response, err := http.ReadResponse(reader, request)
 	if err != nil {
 		return nil, err
 	}
 
-	defer response.Body.Close()
-
 	defer connection.Close()
 
 	app.infoLogger.Println("request forwared and recieved successfully")
 
 	return response, nil
+}
+
+func (app *app) prepareManualResponse(response *http.Response) {
+	// preparing the response to send
+	// 1. body
+	body, err := io.ReadAll(response.Body)
+	app.handleError("failed to read response body", err)
+
+	// 2. headers
+	var headerToSend string
+	for key, value := range response.Header {
+		headerToSend = headerToSend + fmt.Sprintf("%s: %s\r\n", key, strings.Join(value, ","))
+	}
+
+	// 3. final response
+	responseToSend := fmt.Sprintf("%s %s\r\n", response.Proto, response.Status) +
+		headerToSend
+
+	fmt.Println(body, responseToSend)
 }
